af91d3bdec1fc3a85cf67e6f6f18acc56d7f18bc
VitorPandini <valvespandini@gmail.com>
2024-10-19 18:33:42 -0300

    feat: add user entity

---
+++ user-service-api/src/main/java/br/com/padnini/userserviceapi/entity/User.java
@@ -0,0 +1,30 @@
+package br.com.padnini.userserviceapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+
+@AllArgsConstructor @Data @NoArgsConstructor @With
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Set<ProfileEnum> profile;
+
+
+}
