bb42a971d701fb17f0444ec7e859ed1755a7f577
VitorPandini <valvespandini@gmail.com>
2024-10-20 11:05:43 -0300

    feat: add user repository

--- user-service-api/src/main/java/br/com/padnini/userserviceapi/UserServiceApiApplication.java
+++ user-service-api/src/main/java/br/com/padnini/userserviceapi/UserServiceApiApplication.java
@@ -1,13 +1,27 @@
 package br.com.padnini.userserviceapi;
 
+import br.com.padnini.userserviceapi.entity.User;
+import br.com.padnini.userserviceapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import models.enums.ProfileEnum;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
 @SpringBootApplication
-public class UserServiceApiApplication {
+@RequiredArgsConstructor
+public class UserServiceApiApplication implements CommandLineRunner {
+
+    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.save(new User(null, "vipandini", "admin@padnini.com","12345678", Set.of(ProfileEnum.ROLE_ADMIN)));
+    }
 }
