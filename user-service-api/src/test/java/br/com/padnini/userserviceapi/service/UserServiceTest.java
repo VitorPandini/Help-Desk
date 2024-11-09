8de350bff8529c2fa6c2c69d2ec2c6f83fbd0236
VitorPandini <valvespandini@gmail.com>
2024-11-09 17:02:49 -0300

    test: add test to find by id with invalid id user service

--- user-service-api/src/test/java/br/com/padnini/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/padnini/userserviceapi/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package br.com.padnini.userserviceapi.service;
 import br.com.padnini.userserviceapi.entity.User;
 import br.com.padnini.userserviceapi.mapper.UserMapper;
 import br.com.padnini.userserviceapi.repository.UserRepository;
+import exceoptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -42,5 +43,18 @@ class UserServiceTest {
 
         Mockito.verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
         Mockito.verify(userMapper, Mockito.times(1)).fromEntity(Mockito.any(User.class));
+    }
+
+    @Test
+    void whenCallFindByIdWithInvalidIdThenReturnResourceNotFoundException(){
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        try {
+            userService.findById("1");
+        }catch (Exception e){
+            Assertions.assertEquals(ResourceNotFoundException.class, e.getClass());
+        }
+
+        Mockito.verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
+        Mockito.verify(userMapper, Mockito.times(0)).fromEntity(Mockito.any(User.class));
     }
 }
