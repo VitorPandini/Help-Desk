8228acb861017fb2db96d153a2a6f16d9a9ddd3e
VitorPandini <valvespandini@gmail.com>
2024-11-10 13:17:39 -0300

    test: add test to save user with exception

--- user-service-api/src/test/java/br/com/padnini/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/padnini/userserviceapi/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserServiceTest {
@@ -38,57 +41,76 @@ class UserServiceTest {
 
     @Test
     void whenCallFindByIdWithValidIdThenReturnUserResponse(){
-        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(new User()));
-        Mockito.when(userMapper.fromEntity(Mockito.any(User.class))).thenReturn(CreatorUtils.generateMock(UserResponse.class));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(new User()));
+        when(userMapper.fromEntity(Mockito.any(User.class))).thenReturn(CreatorUtils.generateMock(UserResponse.class));
 
         final var response = userService.findById("1");
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(UserResponse.class, response.getClass());
+        assertNotNull(response);
+        assertEquals(UserResponse.class, response.getClass());
 
-        Mockito.verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
-        Mockito.verify(userMapper, Mockito.times(1)).fromEntity(Mockito.any(User.class));
+        verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
+        verify(userMapper, Mockito.times(1)).fromEntity(Mockito.any(User.class));
     }
 
     @Test
     void whenCallFindByIdWithInvalidIdThenReturnResourceNotFoundException(){
-        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
         try {
             userService.findById("1");
         }catch (Exception e){
-            Assertions.assertEquals(ResourceNotFoundException.class, e.getClass());
+            assertEquals(ResourceNotFoundException.class, e.getClass());
         }
 
-        Mockito.verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
-        Mockito.verify(userMapper, Mockito.times(0)).fromEntity(Mockito.any(User.class));
+        verify(userRepository, Mockito.times(1)).findById(Mockito.anyString());
+        verify(userMapper, Mockito.times(0)).fromEntity(Mockito.any(User.class));
     }
 
     @Test
     void whenCallFIndAllThenReturnListOfUserResponse(){
-        Mockito.when(userRepository.findAll()).thenReturn(List.of(new User(),new User()));
-        Mockito.when(userMapper.fromEntity(Mockito.any(User.class))).thenReturn(Mockito.mock(UserResponse.class));
+        when(userRepository.findAll()).thenReturn(List.of(new User(),new User()));
+        when(userMapper.fromEntity(Mockito.any(User.class))).thenReturn(Mockito.mock(UserResponse.class));
 
         final var response = userService.findAll();
 
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(2, response.size());
-        Assertions.assertEquals(UserResponse.class, response.get(0).getClass());
+        assertNotNull(response);
+        assertEquals(2, response.size());
+        assertEquals(UserResponse.class, response.get(0).getClass());
 
-        Mockito.verify(userRepository, Mockito.times(1)).findAll();
-        Mockito.verify(userMapper, Mockito.times(2)).fromEntity(Mockito.any(User.class));
+        verify(userRepository, Mockito.times(1)).findAll();
+        verify(userMapper, Mockito.times(2)).fromEntity(Mockito.any(User.class));
 
     }
 
     @Test
     void whenCallSaveThenSuccess(){
         final var request = CreatorUtils.generateMock(CreateUserRequest.class);
-        Mockito.when(userMapper.fromRequest(Mockito.any())).thenReturn(new User());
-        Mockito.when(encoder.encode(Mockito.anyString())).thenReturn("encoded");
-        Mockito.when(userRepository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+        when(userMapper.fromRequest(Mockito.any())).thenReturn(new User());
+        when(encoder.encode(Mockito.anyString())).thenReturn("encoded");
+        when(userRepository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
 
         userService.save(request);
-        Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any(User.class));
-        Mockito.verify(userRepository, Mockito.times(1)).findByEmail(request.email());
+        verify(userRepository, Mockito.times(1)).save(Mockito.any(User.class));
+        verify(userRepository, Mockito.times(1)).findByEmail(request.email());
 
+    }
+
+    @Test
+    void whenCallSaveWithInvalidEmailThenThrowDataIntegrityViolationException(){
+         final var request = CreatorUtils.generateMock(CreateUserRequest.class);
+         final  var entity =  CreatorUtils.generateMock(User.class);
+         when(userRepository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(entity));
+
+         try{
+             userService.save(request);
+         } catch (Exception e) {
+             assertEquals(DataIntegrityViolationException.class, e.getClass());
+             assertEquals("Email already exists", e.getMessage());
+         }
+
+         verify(userRepository, Mockito.times(1)).findByEmail(request.email());
+         verify(userMapper, Mockito.times(0)).fromRequest(request);
+         verify(userRepository, Mockito.times(1)).findByEmail(request.email());
+         verify(encoder, Mockito.times(0)).encode(request.password());
     }
 
 
