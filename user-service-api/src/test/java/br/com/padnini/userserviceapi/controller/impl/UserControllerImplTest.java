ebc93427315fc5268b54a6bbfc9b01ba0be01367
VitorPandini <valvespandini@gmail.com>
2024-11-12 18:43:18 -0300

    test: add integration to find by id in user controller with not found

--- user-service-api/src/test/java/br/com/padnini/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/br/com/padnini/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -43,6 +44,15 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.email").value(entity.getEmail()));
 
         userRepository.deleteById(userId);
+    }
+
+    @Test
+    void testFindByIdWithNotFoundException() throws Exception {
+        mockMvc.perform(get("api/users/{id}","123"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("User not found"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
     }
 
 }
